3b5f1725f296e4915e6ba9b7fce8fd2aa92055e6
Norbinsh <wowshay@gmail.com>
2020-12-06 17:00:19 +0200

    initial commit

---
+++ test/me.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('String', function() {
+    describe('#split', function() {
+      it('should return an array', function() {
+        assert(Array.isArray('a,b,c'.split(',')))
+      });
+    });
+});

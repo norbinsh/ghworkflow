81982830229047d4abc572044dc47da325e42104
Shay Elmualem <wowshay@Gmail.com>
2021-03-06 09:27:46 +0200

    Update me.js

--- test/me.js
+++ test/me.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 
 describe('String', function() {
     describe('#split', function() {
-      it('should return an array', function() {
+      it('should return an array.', function() {
         assert(Array.isArray('a,b,c'.split(',')))
       });
     });
